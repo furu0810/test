5626f4a851312b03af87c480335e8b18fbf8f81f
furu0810 <shibup89alici810@gmail.com>
2023-09-17 23:10:20 +0900

    四則演算アプリv1.0

--- project/practice_5/test5.java
+++ project/practice_5/test5.java
@@ -1,6 +1,19 @@
 public class test5 {
     public static void main(String[] args) {
 
+        int a = 0;
+        int b = 0;
+
+        b = a++; //後置インクリメント
+
+        System.out.println("代入後にインクリメントしたのでbの値は" + b + "です。"); 
+
+        int c = 0;
+        int d = 0;
+
+        c = ++d; //前置インクリメント
+
+        System.out.println("代入後にインクリメントしたのでcの値は" + c + "です。");
         
     }
 }
---
+++ project/practice_6/test6.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class test6 {
+    public static void main(String[] args) {
+
+        //四則演算のコンソールアプリ
+        //①文字列を入力する
+        //②入力した文字列を数値に変換する
+        //③1-4の選択した数字に応じて四則演算のいずれかで値を計算する
+        
+        System.out.println("計算したい数字を入力してください。");
+
+        Scanner val_1 = new Scanner(System.in);
+        Scanner val_2 = new Scanner(System.in);
+
+        //文字列を小数に変換する
+        double dob_1 = val_1.nextDouble();
+        double dob_2 = val_2.nextDouble();
+
+        //四則演算の計算方式を選択する
+        System.out.println("1-4のいずれかの数字を入力してください。1：足し算、2：引き算、3：掛け算、4：割り算");
+        Scanner val_3 = new Scanner(System.in);
+        int int_1 = val_3.nextInt();
+
+        switch (int_1) {
+            case 1:
+                double addition = dob_1 + dob_2 ;
+                System.out.println("足し算：" + addition);
+                break;
+            case 2:
+                double substraction = dob_1 - dob_2 ;
+                System.out.println("引き算：" + substraction);
+                break;
+            case 3:
+                double multiple = dob_1 * dob_2 ;
+                System.out.println("掛け算：" + multiple);
+                break;
+            case 4:
+                double division = dob_1 / dob_2 ;
+                System.out.println("割り算：" + division);
+                break;            
+        }
+    }
+}
